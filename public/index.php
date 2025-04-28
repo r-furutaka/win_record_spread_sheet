3478a5e6b10f1584fb08cf08ebc084738b180dec
r-furutaka <rettsura@gmail.com>
2025-04-28 21:03:57 +0900

    envが入る

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 
-echo "test no jikan daze!!";
+use Dotenv\Dotenv;
 
+// .envファイルを読み込む
+$dotenv = Dotenv::createImmutable(__DIR__. '/../');
+$dotenv->load();
+
+// 環境変数を表示してみる
+echo "APP_NAME (_ENV): " . $_ENV['APP_NAME'] . "\n";      // MyApp
+
+phpinfo();
